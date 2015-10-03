cd336c6bc1707405e13201a6450f549f26b7462c
sndnvaps <sndnvaps@gmail.com>
2015-10-03 14:16:24 +0800

    Add new method to deal with InsertLogo2QRImage
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

---
+++ QR_Encode_Decode/BitmapHelper.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Drawing;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows;
+namespace QR_Encode_Decode
+{
+    #region 定义结构体，用于确认图片是否需要修改其尺寸
+    struct NeedResize_
+    {
+        Boolean need;
+        int width;
+        int height;
+
+        //是否需要修改图片的大小，true or false
+        public Boolean Need
+        {
+            get
+            {
+                return need;
+            }
+            set
+            {
+                need = value;
+            }
+        }
+
+        //设置宽度
+
+        public int Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                width = value;
+            }
+        }
+
+        //设置高度
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                height = value;
+            }
+        }
+
+    }
+    #endregion
+
+    class BitmapHelper
+    {
+       // private Bitmap bmp;
+
+        #region 定义 BitmapHelper类，用于处理bitmap 类型图片
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject); //用于加载系统函数
+
+        //将bitmap 格式转换为 imagesource 
+        public  ImageSource Bitmap2ImageSource(Bitmap bitmap)
+        {
+            //Bitmap bitmap = icon.ToBitmap();
+            IntPtr hBitmap = bitmap.GetHbitmap();
+            ImageSource wpfBitmap = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                hBitmap,
+                IntPtr.Zero,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+
+            if (!DeleteObject(hBitmap))
+            {
+                throw new System.ComponentModel.Win32Exception();
+            }
+            return wpfBitmap;
+        }
+
+
+        //在将bitmap转换成ImageSource 的时候，也要修改图片的尺寸 
+        //NeedResize_ 是一个结构体 
+        //struct NeedResize_ {
+        // bool need; 是否需要修改图片的尺寸 
+        // int width; 修改图片的宽 
+        //int height; 修改图片的高
+        //} 
+        
+        //bitmap 是要修改的原始图片 
+                
+        public ImageSource Bitmap2ImageSource(Bitmap bitmap,NeedResize_ need)
+        {
+            if (need.Need == false && need.Width == 0&& need.Height == 0) //不需要修改图片的大小
+            {
+                IntPtr hBitmap = bitmap.GetHbitmap();
+                ImageSource wpfBitmap = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    hBitmap,
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+
+                if (!DeleteObject(hBitmap))
+                {
+                    throw new System.ComponentModel.Win32Exception();
+                }
+                return wpfBitmap;
+            } else if (need.Need == true && need.Width > 0 && need.Height > 0 ) //
+            {
+                int width = need.Width;
+                int height = need.Height;
+                Bitmap nbmp = Resize(bitmap,width,height);
+                ImageSource wpfbitmap =  Bitmap2ImageSource(nbmp);
+
+                return wpfbitmap;             
+            } else
+            {
+                throw new ArgumentException("需要修改的图片的尺寸出错或者指定的图片不存在!"); //否则抛出参数异常
+            }
+        }
+
+
+        //此函数用于修改图片的尺寸
+        //para = bmp -> 原始图片
+        //para nwidth -> 需要修改为的宽度
+        //para nwidth -> 需要修改为的高度
+        public Bitmap Resize( Bitmap bmp, int nwidth,int nheight)
+        {
+            if (bmp != null && nwidth > 0 && nheight > 0) //当三个参数都不为空时，修改图片的尺寸
+            {
+                Bitmap nbmp = new Bitmap(bmp, nwidth
+                    , nheight);
+                return (nbmp != null ? nbmp : new Bitmap(100, 100));
+            } else
+            {
+                throw new ArgumentException("需要修改的图片的尺寸出错或者指定的图片不存在!"); //否则抛出参数异常
+            }
+        }
+
+        //将logo 画到二维码上 
+        //Writelogo2QRImage 
+        //paralist 
+        //bitmap1 -> 此为二维码原图
+        //bitmap2 -> logo 图片 
+        //NeedResize_ -> 定义要修改图片的尺寸 
+        public Bitmap WriteLogo2QRImage(Bitmap bmpQR,Bitmap bmpLogo,NeedResize_ need)
+        {
+
+            Bitmap bLogo =bmpLogo; //获取logo图片对象 
+            Bitmap ResizeQRImg = bmpQR;
+
+             if (need.Need == false)
+            {
+                throw new ArgumentException("NeedResize.Need 选项必须要为 true 才可以设置logo的大小");
+            }
+
+            bLogo = new Bitmap(bLogo, need.Width, need.Height);
+             
+            //获取二维码图片的大小尺寸
+            int Y = bmpQR.Height;
+            int X = bmpQR.Width;
+
+
+
+            //坐标点是 （x/2 - logosize/2,y/2 - logosize/2)
+            //Point point = new Point(X / 2 - 15, Y / 2 - 15);//logo图片绘制到二维码上，这里将简单计算一下logo所在的坐标 
+            System.Drawing.Point point = new System.Drawing.Point(X / 2 - need.Width / 2, Y / 2 - need.Height / 2);
+            Graphics g = Graphics.FromImage(ResizeQRImg);//创建一个画布           
+            g.DrawImage(bLogo, point);//将logo图片绘制到二维码图片上    
+
+            return ResizeQRImg;
+
+
+        }
+
+        #endregion
+    }
+}
--- QR_Encode_Decode/MainWindow.xaml.cs
+++ QR_Encode_Decode/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Win32;
+using System.Drawing;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -27,12 +29,13 @@ namespace QR_Encode_Decode
         {
             InitializeComponent();
         }
-        private ImageSource QR_Create()
+
+        Bitmap QRLogo;
+        private Bitmap QR_Create()
         {
-            //BitmapImage bmi;
+            Bitmap bmi;
             //object Obmp;
-            ImageSource ims;
-            
+           
             Encoding encode = Encoding.UTF8;
 
           
@@ -44,34 +47,86 @@ namespace QR_Encode_Decode
             string encodeStr = textBox1.Text.ToString();
 
             QRCodeEncoder qrEncode = new QRCodeEncoder();
-            Bitmap2BitmapImage bmp2bmpimg = new Bitmap2BitmapImage();
+           // BitmapHelper bmp2bmpimg = new BitmapHelper();
 
             qrEncode.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE;//GetEncodeMode(cbEndoeMode.SelectedItem); //选择压缩的方式
             qrEncode.QRCodeScale = 4;//GetQRSize(cbQRSize.SelectedItem);
             qrEncode.QRCodeVersion = 7;//GetQRVersion(cbQRVersion.SelectedItem);   //选择QR版本
             qrEncode.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.M;//GetErrorCorrection(cbErrorCorrection.SelectedItem);  //选择修正率
-            ims = bmp2bmpimg.ChangeBitmapToImageSource (qrEncode.Encode(encodeStr, encode)); //生成二维码，并保存到img 类当中
+              bmi = qrEncode.Encode(encodeStr, encode); //生成二维码，并保存到img 类当中
 
-           // BitmapImage bmi = Obmp as BitmapImage;
+            // BitmapImage bmi = Obmp as BitmapImage;
 
 
-            return ims;
+            return bmi != null ? bmi : new Bitmap(100, 100);
         }
 
-        private void GeneraQR_Click(object sender, RoutedEventArgs e) //此用于生成二维码使用
+        private void GeneraQR_Click(object sender, EventArgs e) //此用于生成二维码使用
         {
 
             try {
                 ImageSource img;
-                img = QR_Create();
+                Bitmap logo = QRLogo;
+                Bitmap QRBitmap;
+                BitmapHelper bitmaphelper = new BitmapHelper();
+                int LogoSize = textBox1.Text != "" ?  Convert.ToInt32(textBox2.Text.ToString()) : 0;
+
+                QRBitmap = QR_Create();
+
+
+                if (logo != null && QRBitmap != null)
+                {
+                    NeedResize_ need = new NeedResize_
+                    {
+                        Need = true,
+                        Width = LogoSize,
+                        Height = LogoSize
+                    };
+
+                    QRBitmap = bitmaphelper.WriteLogo2QRImage(QRBitmap, logo, need); //将logo置入到QR里面
+
+                    img = bitmaphelper.Bitmap2ImageSource(QRBitmap);
+                }
+                else
+                {
+
+
+                    img = bitmaphelper.Bitmap2ImageSource(QRBitmap);
+                }
+
                 image.Source = img;
 
             } 
             catch (Exception es) //用于捕捉所有出错的异常
             {
                 MessageBox.Show(es.Message.ToString(),"程序异常",MessageBoxButton.OK,MessageBoxImage.Error);
-            }      
+            }   
 
+        }
+
+        private void SelQRLogo_Click(object sender, EventArgs e) //用于获取二维码的logo图标
+        {
+           QRLogo = GetLogoFile();
+        }
+
+        private Bitmap GetLogoFile()
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new OpenFileDialog();//文件打开对话框      
+            openFileDialog.Filter = "JPEG|*.jpeg;*.jpg|位图文件|*.bmp|PNG|*.png|所有文件|*.*";//只能打开我们设置的这几类文件 
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string fileName = openFileDialog.FileName;
+                BitmapHelper bitmap2ImageSorce = new BitmapHelper();
+
+                int picheight = (int)image1.Height;
+                int picwidth = (int)image1.Width;
+                System.Drawing.Bitmap newBmp = new Bitmap(fileName);
+                Bitmap bmp = new Bitmap(newBmp, picwidth, picheight);
+                image1.Source = bitmap2ImageSorce.Bitmap2ImageSource(bmp);
+
+                return bmp != null ? bmp : new Bitmap(100, 100);
+            }
+            return new Bitmap(100, 100);
         }
     }
 }
